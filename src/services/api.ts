ecacbb8174d67d79a07ed9ab77eb27bafbfe17a1
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2026-04-14 06:55:37 +0700

    fix: error cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -26,7 +26,7 @@ class ApiTCGService {
   constructor() {
     const isProduction = import.meta.env.PROD;
     const baseURL = isProduction
-      ? "https://corsproxy.io/?https://apitcg.com/api/one-piece"
+      ? "https://api.allorigins.win/raw?url=https://apitcg.com/api/one-piece"
       : import.meta.env.VITE_API_BASE_URL;
     this.api = axios.create({
       baseURL,
